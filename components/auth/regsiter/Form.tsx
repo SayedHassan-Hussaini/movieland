1a772018b7aca05f52b80710600fe0cf42793567
SayedHassan-Hussaini <sh.hussaini1378@gmail.com>
2024-11-27 15:25:42 +0430

    create page for move list and details

--- components/auth/regsiter/Form.tsx
+++ components/auth/regsiter/Form.tsx
@@ -75,7 +75,7 @@ export default function RegisterForm() {
                   <FormControl>
                     <Input
                       className="block sm:leading-6 w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:!ring-xblue-600 sm:text-sm"
-                      placeholder="First Name"
+                      placeholder="last Name"
                       {...field}
                       disabled={isPending}
                     />
