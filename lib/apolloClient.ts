ae8a11fc7df483e46366c56d4a387a91626d35da
SayedHassan-Hussaini <sh.hussaini1378@gmail.com>
2024-12-01 13:51:27 +0430

    add Authorization to movie request

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,8 +1,29 @@
 
-import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const client = new ApolloClient({
+// export default client;
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { getClientAccessToken } from "@/utilities/common";
+
+// Create the HttpLink for the GraphQL endpoint
+const httpLink = new HttpLink({
   uri: process.env.GRAPHQL_ENDPOINT || "https://node-graphql-server.onrender.com",
+});
+
+// Add headers dynamically using setContext
+const authLink =  setContext(async (_, { headers }) => {
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: await getClientAccessToken()
+    },
+  };
+});
+
+// Initialize the Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink), // Combine authLink and httpLink
   cache: new InMemoryCache(),
   ssrMode: true, // Important for server-side rendering
 });
