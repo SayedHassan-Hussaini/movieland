fd6e87fc1c07a177cb001e655d66c459bdd33c13
SayedHassan-Hussaini <sh.hussaini1378@gmail.com>
2024-12-02 09:04:07 +0430

    check layout of movie, user must be login

--- app/(website)/movie/layout.tsx
+++ app/(website)/movie/layout.tsx
@@ -3,12 +3,10 @@
 import { LayoutProps } from "@/types";
 import { useEffect } from "react";
 import { LOGIN_ROUTE } from "@/constant/routes";
-import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { getClientAccessToken } from "@/utilities/common";
 
 export default function RootLayout({ children }: LayoutProps) {
-  const { data: session }: any = useSession();
   const router = useRouter();
   const getSession  =async ()=>{
 
@@ -17,9 +15,9 @@ export default function RootLayout({ children }: LayoutProps) {
       router.push(LOGIN_ROUTE);
     }
   }
-  // useEffect(() => {
-  //   getSession()
-  // }, []);
+  useEffect(() => {
+    getSession()
+  }, []);
 
   return <>{children}</>;
 }
